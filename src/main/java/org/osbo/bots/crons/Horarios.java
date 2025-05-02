ae1e53f9408388e6e9604e01566824297e9bf260
programmercito <aps@outlook.com>
2025-05-01 21:47:16 -0400

    fix: Refactor Horarios class to simplify cron expression parsing

--- src/main/java/org/osbo/bots/crons/Horarios.java
+++ src/main/java/org/osbo/bots/crons/Horarios.java
@@ -13,29 +13,21 @@ public class Horarios {
     private String fin;
     @Value("${telegram.admin}")
     private String adminid;
-    private String i;
-    private String f;
 
-    NqueueForSend queueForSend;
+    private final NqueueForSend queueForSend;
 
     public Horarios(NqueueForSend queueForSend) {
         this.queueForSend = queueForSend;
     }
 
-    Horarios() {
-        this.i = inicio.split(":")[0];
-        this.f = fin.split(":")[0];
-    }
-
-    @Scheduled(cron = "0 0 ${i} * * *")
+    @Scheduled(cron = "0 0 #{T(java.lang.Integer).parseInt('${telegram.horario.inicio}'.split(':')[0])} * * *")
     public void inicio() {
         queueForSend.send(adminid, "El canal de amistad se ha abierto, escribe al bot para publicar !! @datebobot");
     }
 
-    @Scheduled(cron = "0 0 ${f} * * *")
+    @Scheduled(cron = "0 0 #{T(java.lang.Integer).parseInt('${telegram.horario.fin}'.split(':')[0])} * * *")
     public void fin() {
         queueForSend.send(adminid,
                 "El canal de amistad se ha cerrado, no se podra publicar hasta el siguiente horario !! @datebobot");
     }
-
 }
