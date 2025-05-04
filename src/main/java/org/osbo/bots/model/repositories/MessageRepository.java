bae915185e021a743f8a3034e8b0d13b58ceaac5
joa <joa123456789333@mail.com>
2025-05-04 18:53:50 -0400

    fix: Update message expiration time from 30 minutes to 1 hour across multiple classes

--- src/main/java/org/osbo/bots/model/repositories/MessageRepository.java
+++ src/main/java/org/osbo/bots/model/repositories/MessageRepository.java
@@ -9,8 +9,8 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MessageRepository extends JpaRepository<Message, String> {
-    @Query(value = "SELECT COUNT(*) > 0 FROM messages WHERE datetime(expiracion) >= datetime('now','localtime', '-30 minutes') and userid=:user and estado in ('publicado','pendiente')", nativeQuery = true)
-    public int existsMessageInLast30Minutes(String user);
-    @Query(value = "SELECT * FROM messages WHERE datetime(expiracion) < datetime('now','localtime', '-30 minutes') and estado='publicado'", nativeQuery = true)
+    @Query(value = "SELECT COUNT(*) > 0 FROM messages WHERE datetime(expiracion) >= datetime('now','localtime', '-1 hour') and userid=:user and estado in ('publicado','pendiente')", nativeQuery = true)
+    public int existsMessageInLastHour(String user);
+    @Query(value = "SELECT * FROM messages WHERE datetime(expiracion) < datetime('now','localtime', '-1 hour') and estado='publicado'", nativeQuery = true)
     public List<Message> vencidos();
 }
